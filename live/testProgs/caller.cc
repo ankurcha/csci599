71326283d9525e91e9cd25bf8085b473c6c96795
Ankur Chauhan <ankurcha@usc.edu>
2010-04-03 23:43:51 -0700

    Added Live555 library and custom code

--- live/testProgs/caller.cc
+++ live/testProgs/caller.cc
@@ -1,7 +1,8 @@
 /**
  *
  * Ankur's Super Awesome cool utility for world Domination
- * Compile using: g++ -g caller.cc $(pkg-config --libs --cflags libcurl) -o caller
+ * Compile using: 
+ *      g++ -g caller.cc $(pkg-config --libs --cflags libcurl) -o caller
  *
  */
 
@@ -14,12 +15,6 @@
 #include <sys/wait.h>
 #include <curl/curl.h>
 
-#ifdef WIN32
-#include <windows.h>
-#include <string.h>
-#include <conio.h>
-#include <process.h>
-#endif 
 size_t ignore_output(void *ptr, size_t size, size_t nmemb, void *stream){
     return size*nmemb;
 }
@@ -48,6 +43,7 @@ int processSend(int nameID){
     char blockID[100];
     
     char url[256];
+    //strcpy(url,"http://earth.usc.edu/uploads/upload.php");
     strcpy(url,"http://mslab09.usc.edu:22112/cs599/upload.jsp");
     printf("\nAttempting to send temp%d.mp4 to server .....", nameID);
     fflush(stdout);
@@ -69,7 +65,7 @@ int processSend(int nameID){
     curl = curl_easy_init();
     if(curl){
         // Setup Variables for curl mumbo-jumbo!
-        curl_easy_setopt(curl, CURLOPT_URL, "http://mslab09.usc.edu:22112/cs599/upload.jsp");
+        curl_easy_setopt(curl, CURLOPT_URL, url);
         curl_formadd(&post, &last, CURLFORM_COPYNAME, "videoID", CURLFORM_COPYCONTENTS, videoID, CURLFORM_END);
         curl_formadd(&post, &last, CURLFORM_COPYNAME, "blockID", CURLFORM_COPYCONTENTS, blockID, CURLFORM_END);
         curl_formadd(&post, &last, CURLFORM_COPYNAME, "requestType", CURLFORM_COPYCONTENTS, "put", CURLFORM_END);
